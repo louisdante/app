f7de1a8a852308a8bc02cb60f88cf79e06a38fb3
louisdante <mase2kool@yahoo.com.au>
2016-05-18 09:09:15 +0100

    this is my sixth commit

--- app.js
+++ app.js
@@ -1,67 +1,3 @@
-var NotesApplication = function(){
-	this.create = function(note_content){};
-	this.listNotes = function(){};
-	this.search = search(var search_text){
-
-			console.log("Showing results for search ‘[<search_text>]’")
-			console.log("Note ID: [note_id]");
-			console.log("Note ID: [note_id]");
-			console.log("Note ID: [note_id]");
-		
-	};
-	this.delete = function(note_id){
-		if (index > -1) {
-    			array.splice(index, 1);
-			}
-	};
-	this .edit = function edit(note_id, new_content){};
-};
-*/
-
-/*class NotesApplication{
-	var notes =[];
-
-	constructor(author){
-		this.author = author;
-
-	}
-	create(note_content){
-		this.notes.push = note_content;
-		this.author
-	}
-	listNotes(){
-		for (var i = 0; i < notes.length; i++) {
-						
-				console.log("note ID: " + i
-			 	+ "\n"+ notes[i]+"\n\n"+"By Author " + this.author);
-		}	
-	}
-	get(note_id){
-		 
-
-		 if (node_id == key){
-
-		 	return note_content;
-		 }else{
-
-		 	console.log("id not found");
-		 }
-			
-		
-
-	}
-	_search(search_text){
-
-
-		console.log("Showing results for search '[<search_text>]'");
-	}
-	_delete(note_id){}
-	edit(note_id,new_content ){
-
-	}
-
-}*/
-	
 var NotesApplication = function(author){
 
 	this.author = author;
@@ -123,9 +59,3 @@ NotesApplication.prototype.edit(note_id,new_content ){
 	this.notes[note_id] = new_content;
 	}
 
-
-
-/*List<String> strList = new ArrayList<String>();
-strList.add("23-2-2016");
-strList.add("24-2-2016");
-strList.add("25-2-2016");
