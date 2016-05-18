c359c015b32e6389bb707e2e779559f08cf218dd
louisdante <mase2kool@yahoo.com.au>
2016-05-18 09:06:59 +0100

    this is my sixth commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*var NotesApplication = function(){
+var NotesApplication = function(){
 	this.create = function(note_content){};
 	this.listNotes = function(){};
 	this.search = search(var search_text){
@@ -18,7 +18,7 @@
 };
 */
 
-class NotesApplication{
+/*class NotesApplication{
 	var notes =[];
 
 	constructor(author){
@@ -30,13 +30,10 @@ class NotesApplication{
 		this.author
 	}
 	listNotes(){
-		for (var i = Things.length - 1; i >= 0; i--) {
-			
-		
-			for (var i in notes) {
-				console.log("note ID: " + note_id
-			 	+ "\n"+ note_content+"\n\n"+"By Author "+this.author);
-			}
+		for (var i = 0; i < notes.length; i++) {
+						
+				console.log("note ID: " + i
+			 	+ "\n"+ notes[i]+"\n\n"+"By Author " + this.author);
 		}	
 	}
 	get(note_id){
@@ -53,19 +50,82 @@ class NotesApplication{
 		
 
 	}
-	search(search_text){
+	_search(search_text){
 
 
 		console.log("Showing results for search '[<search_text>]'");
 	}
-	delete(note_id){}
+	_delete(note_id){}
 	edit(note_id,new_content ){
 
 	}
 
-}
+}*/
 	
-List<String> strList = new ArrayList<String>();
+var NotesApplication = function(author){
+
+	this.author = author;
+	this.notes = [];
+
+
+}
+NotesApplication.prototype.create(note_content){
+		this.notes.push(note_content);
+		
+	}
+NotesApplication.prototype.listNotes(){
+		for (var i = 0; i < this.notes.length; i++) {
+						
+				console.log("note ID: " + i
+			 	+ "\n"+ this.notes[i]+"\n\n"+"By Author " + this.author);
+		}	
+	}
+NotesApplication.prototype.get(note_id){
+		 
+		 return this.notes[note_id];
+
+		/* if (node_id == key){
+
+		 	return note_content;
+		 }else{
+
+		 	console.log("id not found");
+		 }*/
+			
+		
+
+	}
+NotesApplication.prototype._search(search_text){
+
+
+		console.log("Showing results for search "+search_text);
+
+		for(var i = 0; i < this.notes.length; i++){
+			if(this.notes[i].search(search_text)){
+				
+				console.log("note ID: " + i
+			 	+ "\n"+ this.notes[i]+"\n\n"+"By Author " + this.author);
+			}
+			
+		}
+
+	}
+NotesApplication.prototype._delete(note_id){
+	if(note_id > this.notes.length || note_id < 0){
+		return "the number entered doesnt exit";
+	}else{
+		this.notes.splice(note_id);
+		return "deleted";
+	}
+
+}
+NotesApplication.prototype.edit(note_id,new_content ){
+	this.notes[note_id] = new_content;
+	}
+
+
+
+/*List<String> strList = new ArrayList<String>();
 strList.add("23-2-2016");
 strList.add("24-2-2016");
 strList.add("25-2-2016");
